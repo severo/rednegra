7b26b4f9b25838ac27a5d646388f8ea7a3336092
Sylvain Lesage <severo@rednegra.net>
2021-04-10 09:20:48 +0200

    fix: remove dead code

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,4 @@
 module.exports = function (config) {
   config.addPassthroughCopy({ "./src/_includes/style.css": "style.css" });
-
   config.addPassthroughCopy({ "./src/_includes/favicon": "./" });
-
-  //   config.addShortcode("user", function(name, twitterUsername) {
-  //     return `<div class="user">
-  // <div class="user_name">${name}</div>
-  // <div class="user_twitter">@${twitterUsername}</div>
-  // </div>`;
-  //   });
 };
