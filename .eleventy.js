b72b993ab69f87ac0461ee13423c2159bbc68855
Sylvain Lesage <severo@rednegra.net>
2024-09-27 10:45:30 +0200

    add image

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (config) {
   config.addPassthroughCopy({ "./src/_includes/style.css": "style.css" });
   config.addPassthroughCopy({ "./src/_includes/favicon": "./" });
+  config.addPassthroughCopy("assets");
   config.addPassthroughCopy("CNAME");
 };
